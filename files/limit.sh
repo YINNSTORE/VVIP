25e49cbaab13e8215f8d7aba6399f4f80cc38e0e
YINNSTORE <146443900+YINNSTORE@users.noreply.github.com>
2024-06-01 17:59:25 +0700

    Update limit.sh

--- files/limit.sh
+++ files/limit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/bogelxd/v1/main/"
+REPO="https://raw.githubusercontent.com/YINNSTORE/VVIP/main/"
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}files/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}files/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}files/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
